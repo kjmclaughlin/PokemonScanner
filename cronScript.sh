524949e73641ce8c2f49f7de8ed240fc216f127a
EC2 Default User <ec2-user@ip-172-31-32-155.us-west-2.compute.internal>
2016-07-29 05:06:19 +0000

    Changed the cron to go through all of the scanscripts now that the email issue is fixed

--- cronScript.sh
+++ cronScript.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/PokemonGO
 python scanScript.py
-#sleep(50)
-#python caliScanScript.py
-#sleep(50)
-#python snoqualmieScanScript.py
+python caliScanScript.py
+python snoqualmieScanScript.py
